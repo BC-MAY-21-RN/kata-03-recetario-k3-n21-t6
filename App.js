ab01979e9e84f16b7041e3b0deceb5f411fd9e6a
Nando San Miguel <mythemc@gmail.com>
2021-11-30 19:06:00 -0600

    Add search input, add image.
    Co-authored-by: Darkboy5120 <hmaldonado0@ucol.mx>
    Co-authored-by: Hugoneytor <halvarez0@ucol.mx>

--- App.js
+++ App.js
@@ -10,33 +10,80 @@ Co-authored-by: Darkboy5120 <hmaldonado0@ucol.mx>
 Co-authored-by: Hugoneytor <halvarez0@ucol.mx> 
  */
 
+import { whileStatement } from '@babel/types';
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TextInput, Image} from 'react-native';
+import Recipes from './src/assets/data.json';
+
+
+
+const Recipe = ({url, name}) => {
+  return (
+    <View style={styles.recipeContainer}>
+      <Image style={styles.recipeImage} source={url} />
+      <Text style={styles.recipeName}>{name}</Text>
+    </View>
+  );
+};
 
 const InputSearch = () => {
   return (
     <View>
-      <Text>
-        Hola pepe
-      </Text>
+      <TextInput
+        placeholder="What do you want to eat?"
+        placeholderTextColor="white"
+        style={styles.search}/>
     </View>
   );
 };
 
 const App = () => {
+  //const renderRecipes = ({item}) => <Recipe url='./img/recipes/dish0.jpeg' name={item.name}/>;
+
   return (
     <View style={styles.body}>
       <InputSearch/>
+      {/* <Recipe source={require('./src/assets/images/dish0.jpeg')} name='Hey'/> */}
+      {/* <FlatList
+        data={Recipes}
+        renderItem={renderRecipes}
+        keyExtractor={recipe => recipe.id}
+      /> */}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   body: {
-    backgroundColor: 'rgb(50, 50, 50)',
+    backgroundColor: '#282828',
     padding: 20,
     flex: 1,
   },
+  recipeContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: 20,
+  },
+  recipeImage: {
+    borderRadius: 25,
+    width: 120,
+    height: 120,
+    
+  },
+  recipeName: {
+    fontSize: 24,
+    color: 'white',
+    paddingTop: 20,
+  },
+  search: {
+    width: '95%',
+    height: 40,
+    padding: 10,
+    backgroundColor: '#343435',
+    color: 'white',
+    margin: 10,
+    borderRadius: 10
+  }
 });
 
 export default App;
